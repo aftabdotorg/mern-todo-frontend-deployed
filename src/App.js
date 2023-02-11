5ea918847ff70f3ff8179e8db0ac5bab32054581
aftabdotorg <aft4b.akhand@gmail.com>
2023-02-11 18:40:04 +0530

    deployed

---
+++ src/App.js
@@ -0,0 +1,141 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+const API_BASE = `https://merntodobackend.onrender.com`;
+
+function App() {
+  const [todos, setTodos] = useState([]); // [initial state, updater functionn]
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState('');
+
+  useEffect(() => {
+    GetTodos();
+  }, []); // [] dependency
+
+  const GetTodos = () => {
+    fetch(`${API_BASE}/todos`)
+      .then((res) => res.json()) //send response to json
+      .then((data) => setTodos(data))
+      .catch((err) => console.error('Error:', err));
+  };
+
+  //trials
+
+  // const addTodo = () => {
+  //   return fetch(API_BASE + '/todo/new', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       text: newTodo,
+  //     }),
+  //   })
+  //     .then((res) => res.json())
+  //     .then((data) => setTodos([...todos, data]));
+  // };
+
+  // const handleAdd = async () => {
+  //   await addTodo();
+  //   setPopupActive(false);
+  // };
+
+  //trial ends
+
+  //toggle complete
+  const completeTodo = async (id) => {
+    const data = await fetch(`${API_BASE}/todos/complete/${id}`).then((res) =>
+      res.json()
+    );
+    setTodos((todos) =>
+      todos.map((todo) => {
+        if (todo._id === data._id) {
+          todo.complete = data.complete;
+        }
+        return todo;
+      })
+    );
+  };
+
+  const deleteTodo = async (id) => {
+    const data = await fetch(API_BASE + '/todos/delete/' + id, {
+      method: 'DELETE',
+    }).then((res) => res.json());
+
+    setTodos((todos) => todos.filter((todo) => todo._id !== data._id));
+  };
+
+  const addTodo = async () => {
+    const data = await fetch(API_BASE + '/todo/new', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        text: newTodo,
+      }),
+    }).then((res) => res.json());
+
+    setTodos([...todos, data]);
+
+    // console.log(data);
+    setPopupActive(false);
+    setNewTodo("")
+  };
+
+  return (
+    <div className="App">
+      <h1>Welcome, Aftab</h1>
+      <h4>your tasks</h4>
+      <div className="todos">
+        { todos.length > 0 ?  todos.map((todo) => (
+          <div
+            className={`todo ${todo.complete ? 'is-complete' : ''}`}
+            key={todo._id}
+            onClick={() => completeTodo(todo._id)}
+          >
+            <div className="checkbox"></div>
+            <div className="text">{todo.text}</div>
+            <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>
+              x
+            </div>
+          </div>
+        )) : (
+          <p>No Pending Tasks</p>
+        ) }
+      </div>
+      <div className="addPopup" onClick={() => setPopupActive(true)}>
+        +
+      </div>
+      {popupActive ? (
+        <div className="popup">
+          <div className="closePopup" onClick={() => setPopupActive(false)}>
+            x
+          </div>
+          <div className="content">
+            <h3>ADD TASKS</h3>
+            <input
+              type="text"
+              className="add-todo-input"
+              // onChange={(e) => setNewTodo(e.target.value)}
+              onChange={(e) => setNewTodo(e.target.value)}
+              value={newTodo}
+            />
+            {newTodo}
+            <div
+              className="button"
+               onClick={addTodo}
+              // onClick={handleAdd}
+            >
+              Create Task
+            </div>
+          </div>
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  );
+}
+
+export default App;
