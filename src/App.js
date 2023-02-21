698a44d4bb5576f40c750b5275030adcc1dbd236
aftabdotorg <aft4b.akhand@gmail.com>
2023-02-21 20:13:56 +0530

    minor animations and bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import Spinner from './Spinner';
+import FlipMove from 'react-flip-move';
 
 const API_BASE = `https://merntodobackend.onrender.com`;
 
@@ -7,40 +9,25 @@ function App() {
   const [todos, setTodos] = useState([]); // [initial state, updater functionn]
   const [popupActive, setPopupActive] = useState(false);
   const [newTodo, setNewTodo] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     GetTodos();
   }, []); // [] dependency
 
   const GetTodos = () => {
+    setLoading(true);
     fetch(`${API_BASE}/todos`)
       .then((res) => res.json()) //send response to json
-      .then((data) => setTodos(data))
-      .catch((err) => console.error('Error:', err));
+      .then((data) => {
+        setTodos(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error('Error:', err);
+        setLoading(false);
+      });
   };
-
-  //trials
-
-  // const addTodo = () => {
-  //   return fetch(API_BASE + '/todo/new', {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({
-  //       text: newTodo,
-  //     }),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => setTodos([...todos, data]));
-  // };
-
-  // const handleAdd = async () => {
-  //   await addTodo();
-  //   setPopupActive(false);
-  // };
-
-  //trial ends
 
   //toggle complete
   const completeTodo = async (id) => {
@@ -80,7 +67,7 @@ function App() {
 
     // console.log(data);
     setPopupActive(false);
-    setNewTodo("")
+    setNewTodo('');
   };
 
   return (
@@ -88,21 +75,42 @@ function App() {
       <h1>Welcome, Aftab</h1>
       <h4>your tasks</h4>
       <div className="todos">
-        { todos.length > 0 ?  todos.map((todo) => (
-          <div
-            className={`todo ${todo.complete ? 'is-complete' : ''}`}
-            key={todo._id}
-            onClick={() => completeTodo(todo._id)}
-          >
-            <div className="checkbox"></div>
-            <div className="text">{todo.text}</div>
-            <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>
-              x
-            </div>
-          </div>
-        )) : (
-          <p>No Pending Tasks</p>
-        ) }
+        <FlipMove>
+          {loading ? (
+            <h2
+              style={{
+                display: 'grid',
+                justifyContent: 'center',
+                color: 'blueviolet',
+                textTransform: 'uppercase',
+              }}
+            >
+              {' '}
+              <Spinner /> <br />
+              please wait while todos are being fetched, <br />
+              This may take time depending upon Api
+            </h2>
+          ) : todos.length > 0 ? (
+            todos.map((todo) => (
+              <div
+                className={`todo ${todo.complete ? 'is-complete' : ''}`}
+                key={todo._id}
+                onClick={() => completeTodo(todo._id)}
+              >
+                <div className="checkbox"></div>
+                <div className="text">{todo.text}</div>
+                <div
+                  className="delete-todo"
+                  onClick={() => deleteTodo(todo._id)}
+                >
+                  x
+                </div>
+              </div>
+            ))
+          ) : (
+            <p>No Pending Tasks</p>
+          )}
+        </FlipMove>
       </div>
       <div className="addPopup" onClick={() => setPopupActive(true)}>
         +
@@ -121,10 +129,9 @@ function App() {
               onChange={(e) => setNewTodo(e.target.value)}
               value={newTodo}
             />
-            {newTodo}
             <div
               className="button"
-               onClick={addTodo}
+              onClick={addTodo}
               // onClick={handleAdd}
             >
               Create Task
